e360a38bd3043345ac9bf92f157e1a5b3a035801
Tara Nelson & Maureen Dugan <student@epicodus.com>
2014-03-31 10:05:22 -0700

    initial framework and methods for wikipages

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+
+  def new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.create(:name => params[:name], :email => params[:email], :phone => params[:phone])
+    render('contacts/success.html.erb')
+  end
+
+  def index
+    @contacts = Contact.all
+    render('contacts/index.html.erb')
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render('contacts/show.html.erb')
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = 'efd3d4dd86e856489073cc78060c370909a7a714d9ee40d01f558aba687a1c1d742015bcddd5856142f6b3014ee6ed8b215b63aa449b29ee04169dab9e1b63c6'
