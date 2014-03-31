a3853e2e26b4c307c5d718f4ac3230bb65780de0
Tara Nelson & Maureen Dugan <student@epicodus.com>
2014-03-31 10:39:17 -0700

    complete CRUD and REST functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,17 @@
 class ContactsController < ApplicationController
 
   def new
+    @contact = Contact.new
     render('contacts/new.html.erb')
   end
 
   def create
-    @contact = Contact.create(:name => params[:name], :email => params[:email], :phone => params[:phone])
-    render('contacts/success.html.erb')
+    @contact = Contact.new(:name => params[:name], :email => params[:email], :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
   end
 
   def index
@@ -17,5 +22,25 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
     render('contacts/show.html.erb')
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name], :email => params[:email], :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
   end
 end
